1c4b8e3fccbdfb2ebfc36842a8776ec266467b2f
Igor Rybyakov <rybyakovigor@gmail.com>
2020-10-07 16:06:45 +0500

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShipDetailsComponent } from './ship-details/ship-details.component';
+import { ShipListComponent } from './ship-list/ship-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShipListComponent
+  },
+  {
+    path: ':id',
+    component: ShipDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
