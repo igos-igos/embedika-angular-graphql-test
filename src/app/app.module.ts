1d90d50f1876b91f7ead7f5d16ade59568da37b8
Igor Rybyakov <rybyakovigor@gmail.com>
2020-10-07 16:04:38 +0500

    add components for list, detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShipListComponent } from './ship-list/ship-list.component';
+import { ShipDetailsComponent } from './ship-details/ship-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShipListComponent,
+    ShipDetailsComponent
   ],
   imports: [
     BrowserModule,
