782b6cb145d7f61b0e95561409d1638ce0a93e65
Igor Rybyakov <rybyakovigor@gmail.com>
2020-10-07 16:52:32 +0500

    add shiplist query

--- src/app/ship-list/ship-list.component.ts
+++ src/app/ship-list/ship-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators'
+import { ShipsGQL } from '../services/spacexGraphql.service';
 
 @Component({
   selector: 'app-ship-list',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly shipListService: ShipsGQL) { }
+
+  shipList = this.shipListService
+    .fetch()
+    .pipe(
+      map(res => res.data)
+    )
 
   ngOnInit() {
   }
