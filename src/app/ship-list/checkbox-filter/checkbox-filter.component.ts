ac7a8889333b4ced57329a6833c7454ca5d25960
Igor Rybyakov <rybyakovigor@gmail.com>
2020-10-14 11:55:08 +0500

    finish filter block

--- src/app/ship-list/checkbox-filter/checkbox-filter.component.ts
+++ src/app/ship-list/checkbox-filter/checkbox-filter.component.ts
@@ -8,9 +8,20 @@ import { FiltersService } from 'src/app/services/filters.service';
 })
 export class CheckboxFilterComponent implements OnInit {
 
+  checkboxCount = 0
+
   constructor(public filters: FiltersService) { }
 
   ngOnInit(): void {
   }
 
+  toggleCheckboxCount(event) {
+    if (this.checkboxCount === 0 && !event.target.checked) {
+      return
+    } else if (event.target.checked) {
+      this.checkboxCount += 1
+    } else {
+      this.checkboxCount -= 1
+    }
+  }
 }
